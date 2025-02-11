495a0f3152654498b81a1c7915a601e66e0fd20a
Jenous <62263579+EaGLenok@users.noreply.github.com>
2025-02-11 16:59:14 +0100

    Setup project

---
+++ src/frontend/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Container, Typography } from "@mui/material";
+
+const App: React.FC = () => {
+
+    return (
+        <Container style={{ textAlign: "center", marginTop: "50px" }}>
+            <Typography variant="h4" gutterBottom>
+                njllljkljklljk hy
+            </Typography>
+        </Container>
+    );
+};
+
+export default App;
